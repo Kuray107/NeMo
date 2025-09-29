c660af1f815e70011bb3dd77e06cce3f028bb837
rickyen1011 <rick.yen@gatech.edu>
2025-09-29 18:53:32 +0000

    restoring transformer.py and transformer_utils.py to origin

--- nemo/collections/asr/modules/transformer/transformer.py
+++ nemo/collections/asr/modules/transformer/transformer.py
@@ -20,7 +20,7 @@ from omegaconf.omegaconf import MISSING, DictConfig
 
 from nemo.collections.asr.modules.transformer.decoder_module import DecoderModule
 from nemo.collections.asr.modules.transformer.encoder_module import EncoderModule
-from nemo.collections.asr.modules.transformer.transformer_decoders import TransformerDecoder, TransformerDecoderAdapter, TransformerDecoderDDMS
+from nemo.collections.asr.modules.transformer.transformer_decoders import TransformerDecoder, TransformerDecoderAdapter
 from nemo.collections.asr.modules.transformer.transformer_encoders import TransformerEncoder
 from nemo.collections.asr.modules.transformer.transformer_modules import TransformerEmbedding
 from nemo.collections.asr.parts.submodules.adapters.attention_adapter_mixin import AttentionAdapterModuleMixin
@@ -311,77 +311,6 @@ class TransformerDecoderNMAdapter(TransformerDecoderNM, adapter_mixins.AdapterMo
         cfg = adapter_utils.update_adapter_cfg_input_dim(self, cfg, module_dim=self._hidden_size)
         return cfg
 
-import einops
-from nemo.collections.asr.parts.submodules.discrete_diffusion_scheduler import get_noise_scheduler
-class TransformerDecoderDDMSNM(TransformerDecoderNM):
-    DECODER_TYPE: type = TransformerDecoderDDMS
-
-    def __init__(
-        self,
-        vocab_size: int,
-        hidden_size: int,
-        num_layers: int,
-        inner_size: int,
-        num_attention_heads: int,
-        max_sequence_length: int = 512,
-        num_token_types: int = 2,
-        embedding_dropout: float = 0.0,
-        learn_positional_encodings: bool = False,
-        ffn_dropout: float = 0.0,
-        attn_score_dropout: float = 0.0,
-        attn_layer_dropout: float = 0.0,
-        hidden_act: str = 'relu',
-        pre_ln: bool = False,
-        pre_ln_final_layer_norm: bool = True,
-        cfg = None
-    ):
-        super().__init__(
-            vocab_size, hidden_size, num_layers, inner_size, num_attention_heads, max_sequence_length, num_token_types, embedding_dropout,
-            learn_positional_encodings, ffn_dropout, attn_score_dropout, attn_layer_dropout, hidden_act, pre_ln, pre_ln_final_layer_norm
-        )
-
-        self.noise_schedule = get_noise_scheduler(cfg)
-
-    def _prepare_decoder_input_and_target(self, token_ids):
-
-        time = self.noise_schedule.sample_time(batch_size=token_ids.size(0), device=token_ids.device)
-        mask_prob = 1 - time
-        mask_prob = einops.repeat(mask_prob, 'b -> b t', t = token_ids.size(1))
-        will_mask = torch.bernoulli(mask_prob).to(dtype=torch.bool).to(token_ids.device)
-        masked_token_ids = torch.where(will_mask, 0, token_ids)
-
-        return masked_token_ids
-    
-    def forward(
-        self,
-        input_ids,
-        decoder_mask,
-        encoder_embeddings,
-        encoder_mask,
-        decoder_mems=None,
-    ):
-        start_pos = 0
-        if decoder_mems is not None:
-            start_pos = input_ids.shape[1] - 1
-            input_ids = input_ids[:, -1:]
-            decoder_mask = decoder_mask[:, -1:]
-            decoder_mems = torch.transpose(decoder_mems, 0, 1)
-        
-        # Modify the input_ids by masking out some of the tokens
-        input_ids = self._prepare_decoder_input_and_target(input_ids)
-        decoder_embeddings = self._embedding(input_ids=input_ids, start_pos=start_pos)
-        decoder_hidden_states = self._decoder(
-            decoder_states=decoder_embeddings,
-            decoder_mask=decoder_mask,
-            encoder_states=encoder_embeddings,
-            encoder_mask=encoder_mask,
-            decoder_mems_list=decoder_mems,
-            return_mems=self.return_mems,
-            return_mems_as_list=False,
-        )
-        if self.return_mems:
-            decoder_hidden_states = torch.transpose(decoder_hidden_states, 0, 1)
-        return decoder_hidden_states
 
 """
 Register any additional information
--- nemo/collections/asr/modules/transformer/transformer_utils.py
+++ nemo/collections/asr/modules/transformer/transformer_utils.py
@@ -17,7 +17,7 @@ from typing import Optional, Union
 
 from omegaconf.dictconfig import DictConfig
 
-from nemo.collections.asr.modules.transformer.transformer import TransformerDecoderNM, TransformerEncoderNM, TransformerDecoderDDMSNM
+from nemo.collections.asr.modules.transformer.transformer import TransformerDecoderNM, TransformerEncoderNM
 from nemo.collections.asr.modules.transformer.transformer_bottleneck import TransformerBottleneckEncoderNM
 
 __all__ = ['get_nemo_transformer']
@@ -113,44 +113,7 @@ def get_nemo_transformer(
         else:
             raise ValueError(f"Unknown arch = {arch}")
     else:
-        # if cfg.get('dec_type') == 'ddms':
-        #     model = TransformerDecoderDDMSNM(
-        #         vocab_size=cfg.get('vocab_size'),
-        #         hidden_size=cfg.get('hidden_size'),
-        #         num_layers=cfg.get('num_layers'),
-        #         inner_size=cfg.get('inner_size'),
-        #         max_sequence_length=cfg.get('max_sequence_length', 512),
-        #         embedding_dropout=cfg.get('embedding_dropout', 0.0),
-        #         learn_positional_encodings=cfg.get('learn_positional_encodings', False),
-        #         num_attention_heads=cfg.get('num_attention_heads'),
-        #         ffn_dropout=cfg.get('ffn_dropout', 0.0),
-        #         attn_score_dropout=cfg.get('attn_score_dropout', 0.0),
-        #         attn_layer_dropout=cfg.get('attn_layer_dropout', 0.0),
-        #         hidden_act=cfg.get('hidden_act', 'relu'),
-        #         pre_ln=cfg.get('pre_ln', False),
-        #         pre_ln_final_layer_norm=pre_ln_final_layer_norm,
-        #         num_token_types=cfg.get('num_token_types', 2),
-        #         cfg=cfg
-        #     )
 
-        # else:
-        #     model = TransformerDecoderNM(
-        #         vocab_size=cfg.get('vocab_size'),
-        #         hidden_size=cfg.get('hidden_size'),
-        #         num_layers=cfg.get('num_layers'),
-        #         inner_size=cfg.get('inner_size'),
-        #         max_sequence_length=cfg.get('max_sequence_length', 512),
-        #         embedding_dropout=cfg.get('embedding_dropout', 0.0),
-        #         learn_positional_encodings=cfg.get('learn_positional_encodings', False),
-        #         num_attention_heads=cfg.get('num_attention_heads'),
-        #         ffn_dropout=cfg.get('ffn_dropout', 0.0),
-        #         attn_score_dropout=cfg.get('attn_score_dropout', 0.0),
-        #         attn_layer_dropout=cfg.get('attn_layer_dropout', 0.0),
-        #         hidden_act=cfg.get('hidden_act', 'relu'),
-        #         pre_ln=cfg.get('pre_ln', False),
-        #         pre_ln_final_layer_norm=pre_ln_final_layer_norm,
-        #         num_token_types=cfg.get('num_token_types', 2),
-        #     )
         model = TransformerDecoderNM(
             vocab_size=cfg.get('vocab_size'),
             hidden_size=cfg.get('hidden_size'),
